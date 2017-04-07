6f3894482e9b0b27b5276f0ae74bec5952eb8bec
acorea2541 <abrahamcoreadiaz@Abrahams-MacBook-Pro.local>
2017-04-07 15:08:31 -0700

    Add WebPage

---
+++ WebPage/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(1275,700);
+  background("#beefed");
+  textAlign(CENTER);
+  textFont("Georgia");
+  textSize(32);
+  text("Lick Wilmerding Course Selection", width/2, 50); //title
+  
+}
+
+function draw() {
+  
+}
+
