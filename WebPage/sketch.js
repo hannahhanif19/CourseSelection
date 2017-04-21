6f39730d0eddf451bfd23721834e2cf492e40606
acorea2541 <abrahamcoreadiaz@Abrahams-MacBook-Pro.local>
2017-04-21 10:47:14 -0700

    Finished Departments Page
    All Subjects have own page
    -math
    -history
    -english
    -world language
    -elective
    -science

--- WebPage/sketch.js
+++ WebPage/sketch.js
@@ -6,6 +6,10 @@ var button4;
 var button5;
 var math;
 var english;
+var science;
+var historyn;
+var wlang;
+var electives;
 
 function setup() {
   createCanvas(1275,700);
@@ -114,11 +118,45 @@ function dePartments() {
   button4.hide();
   button5.hide();
   math = createButton('Math');
-  math.position(width/2-500, 300);
+  math.position(100, 100);
   math.size(300,300);
-  math.style("font-size", "22px")
-  math.style("background-color", "#cfe2f3")
+  math.style("font-size", "22px");
+  math.style("background-color", "#cfe2f3");
   math.mousePressed(mathp);
+  english = createButton('English');
+  english.position(450, 100);
+  english.size(300,300);
+  english.style("font-size", "22px")
+  english.style("background-color", "#cfe2f3")
+  english.mousePressed(englishp);
+  science = createButton('Science');
+  science.position(800, 100);
+  science.size(300,300);
+  science.style("font-size", "22px")
+  science.style("background-color", "#cfe2f3")
+  science.mousePressed(sciencep);
+  
+  historyn = createButton('History');
+  historyn.position(100, 450);
+  historyn.size(300,300);
+  historyn.style("font-size", "22px")
+  historyn.style("background-color", "#cfe2f3")
+  historyn.mousePressed(historyp);
+  
+  wlang = createButton('World Language');
+  wlang.position(450, 450);
+  wlang.size(300,300);
+  wlang.style("font-size", "22px")
+  wlang.style("background-color", "#cfe2f3")
+  wlang.mousePressed(wlangp);
+  
+  elective = createButton('Elective');
+  elective.position(800, 450);
+  elective.size(300,300);
+  elective.style("font-size", "22px")
+  elective.style("background-color", "#cfe2f3")
+  elective.mousePressed(electivep);
+  
   button1 = createButton('Go Back');
   button1.position(25, 25);
   button1.size(100,35);
@@ -129,12 +167,97 @@ function dePartments() {
 function mathp() {
   createCanvas(1275,700);
   math.hide();
+  english.hide();
+  science.hide();
+  historyn.hide();
+  wlang.hide();
+  elective.hide();
   button1.hide();
   button6 = createButton('Go Back');
   button6.position(25, 25);
   button6.size(100,35);
   button6.mousePressed(dePartments);
   button6.mousePressed(hideButton6);
+}
+
+function englishp() {
+  createCanvas(1275,700);
+  math.hide();
+  english.hide();
+  science.hide();
+  historyn.hide();
+  wlang.hide();
+  elective.hide();
+  button1.hide();
+  button7 = createButton('Go Back');
+  button7.position(25, 25);
+  button7.size(100,35);
+  button7.mousePressed(dePartments);
+  button7.mousePressed(hideButton7);
+}
+
+function sciencep() {
+  createCanvas(1275,700);
+  math.hide();
+  english.hide();
+  science.hide();
+  historyn.hide();
+  wlang.hide();
+  elective.hide();
+  button1.hide();
+  button8 = createButton('Go Back');
+  button8.position(25, 25);
+  button8.size(100,35);
+  button8.mousePressed(dePartments);
+  button8.mousePressed(hideButton8);
+}
+
+function historyp() {
+  createCanvas(1275,700);
+  math.hide();
+  english.hide();
+  science.hide();
+  historyn.hide();
+  wlang.hide();
+  elective.hide();
+  button1.hide();
+  button9 = createButton('Go Back');
+  button9.position(25, 25);
+  button9.size(100,35);
+  button9.mousePressed(dePartments);
+  button9.mousePressed(hideButton9);
+}
+
+function wlangp() {
+  createCanvas(1275,700);
+  math.hide();
+  english.hide();
+  science.hide();
+  historyn.hide();
+  wlang.hide();
+  elective.hide();
+  button1.hide();
+  button10 = createButton('Go Back');
+  button10.position(25, 25);
+  button10.size(100,35);
+  button10.mousePressed(dePartments);
+  button10.mousePressed(hideButton10);
+}
+
+function electivep() {
+  createCanvas(1275,700);
+  math.hide();
+  english.hide();
+  science.hide();
+  historyn.hide();
+  wlang.hide();
+  elective.hide();
+  button1.hide();
+  button11 = createButton('Go Back');
+  button11.position(25, 25);
+  button11.size(100,35);
+  button11.mousePressed(dePartments);
+  button11.mousePressed(hideButton11);
 }
 
 function planSchedule() {
@@ -153,8 +276,34 @@ function planSchedule() {
 
 function hideButton1() {
   button1.hide();
+  math.hide();
+  english.hide();
+  science.hide();
+  historyn.hide();
+  wlang.hide();
+  elective.hide();
 }
 
 function hideButton6() {
   button6.hide();
+}
+
+function hideButton7() {
+	button7.hide();
+}
+
+function hideButton8() {
+	button8.hide();
+}
+
+function hideButton9() {
+	button9.hide();
+}
+
+function hideButton10() {
+	button10.hide();
+}
+
+function hideButton11() {
+	button11.hide();
 }
