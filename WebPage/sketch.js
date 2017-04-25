b5a8dae5a3ae0e39afca5943e2f79ce940a8ec2c
acorea2541 <abrahamcoreadiaz@Abrahams-MacBook-Pro.local>
2017-04-25 10:04:03 -0700

    Created Math Info Page
    Using Hannah’s html/jquery expandable content file, I found a way to
    display it at the correct time. Hannah and I both tried many different
    methods until we found a way that worked! The problem is that we only
    have one html file. We are currently working on figuring out how to
    link between html files.

--- WebPage/sketch.js
+++ WebPage/sketch.js
@@ -1,3 +1,6 @@
+var canvas;
+var canvas2;
+var canvas3;
 var button;
 var button1;
 var button2;
@@ -6,15 +9,17 @@ var button4;
 var button5;
 var math;
 var english;
+var button6;
+var overview;
 var science;
 var historyn;
 var wlang;
 var electives;
 var button6;
 
-
 function setup() {
-  createCanvas(1275,700);
+  canvas = createCanvas(1275,700);
+  canvas.position(0,0);
   background("#beefed");
   textAlign(CENTER);
   textFont("Georgia");
@@ -40,29 +45,34 @@ function setup() {
   textFont("Georgia");
   textSize(18);
   fill(0)
-  text("This is the course selection page that allows ……… (overview of web page)", width/2, 180); //course page overview
+  textAlign(CENTER);
+  textStyle(ITALIC);
+  overview = "This is the new and improved course selection page that allows students to find everything they are looking for during course signups all in one place. This page includes a login and sign up section so that students can save their work, a departments page that includes all the subjects and the courses offered at Lick, and a scheduling page which references to graduation requirements and an interactive course planner for the upcoming year. "; //course page overview
+  text(overview, width/2-350, 140, 680, 200);
+
   
   button3 = createButton('USER INFO');
-  button3.position(width/2-500, 300);
+  button3.position(width/2-500, 310);
   button3.size(300,300);
   button3.style("font-size", "22px")
   button3.style("background-color", "#cfe2f3")
   button3.mousePressed(userInfo);
   
   button4 = createButton('DEPARTMENTS');
-  button4.position(width/2-150, 300);
+  button4.position(width/2-150, 310);
   button4.size(300,300);
   button4.style("font-size", "22px")
   button4.style("background-color", "#cfe2f3")
   button4.mousePressed(dePartments);
   
   button5 = createButton('PLAN YOUR SCHEDULE');
-  button5.position(width/2 +200, 300);
+  button5.position(width/2 +200, 310);
   button5.size(300,300);
   button5.style("font-size", "22px")
   button5.style("background-color", "#cfe2f3")
   button5.mousePressed(planSchedule);
-
+  
+  
 }
 
 function draw() {
@@ -113,7 +123,9 @@ function userInfo() {
 }
 
 function dePartments() {
-  createCanvas(1275,700);
+  canvas2 = createCanvas(1275,700);
+  background("#beefed");
+  canvas2.position(0,0);
   button.hide();
   button2.hide();
   button3.hide();
@@ -121,6 +133,7 @@ function dePartments() {
   button5.hide();
   
   math = createButton('Math');
+
   math.position(100, 100);
   math.size(300,300);
   math.style("font-size", "22px");
@@ -168,19 +181,22 @@ function dePartments() {
 }
 
 function mathp() {
-  createCanvas(1275,700);
+  noCanvas();
   math.hide();
   english.hide();
   science.hide();
   historyn.hide();
   wlang.hide();
   elective.hide();
+
   button1.hide();
   button6 = createButton('Go Back');
   button6.position(25, 25);
   button6.size(100,35);
   button6.mousePressed(dePartments);
   button6.mousePressed(hideButton6);
+  
+  
 }
 
 function englishp() {
@@ -280,7 +296,7 @@ function planSchedule() {
 function hideButton1() {
   button1.hide();
   math.hide();
-  english.hide();
+  //english.hide();
   science.hide();
   historyn.hide();
   wlang.hide();
